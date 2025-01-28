5556b1eae932b2ff85992147d54ddca8eff94877
Anjali Narang <s1328134@monmouth.edu>
2025-01-28 17:24:02 -0500

    finishing touches

--- exercise 1/perlex1.pl
+++ exercise 1/perlex1.pl
@@ -1,5 +1,5 @@
-# Anjali Narang
-# CS-371
+# Anjali Narang s1328134
+# CS 371-01
 # Scripting Languages
 # Spring 2025
 
@@ -8,22 +8,22 @@
  # That is, find and print the number of words in english.sorted that are one character long, 
  # two characters long, three characters long and so on. 
 
-my $inputFile = "english.sorted"; # since it's declared outside of a loop/such, it will exist throughout 
-# the file
+my $filename = "english.sorted";
 
-open(my $fh, "<", $inputFile) or die "Cannot open $inputFile"; # open in read mode or print error if can't open
+# open in read mode or print error if can't open
+open(my $file, "<", $filename) or die "Cannot open $filename";
 
-# going to make a hash that maps length as key to count. whenever we find another occurrence
-# of a length, we add to the count.
+my $wordLengths; # hash to map lengths as key to count
 
-my $wordLengths; # hash
-
-while (my $line = <$fh>) { # $line will be local to this block b/c it's a loop
-   chomp($line); # to get rid of new line character at end.. otherwise there are no 1 letter words
+while (my $line = <$file>) {
+   chomp($line); # remove newline character at the end
    $wordLengths{length($line)}++;  # add 1 to the corresponding count in hash
 }
 
-print "length 3: ".$wordLengths{3}."\n"; # test
-print "length 1: ".$wordLengths{1}; # test; outputs 26 b/c the file has all 26 singular letters
+close($file) # do not need the file anymore
 
-# close the file?
+print “Word length \t Occurrences \n\n”; # column headers
+
+foreach my $length (keys %wordLengths) { # for each key in the hash
+	print “$length \t\t $wordLengths{$length} \n”;
+}
